7a9395a6559d1ca1ab51231d38ae18e108bfccf8
Stone Coder <stonecodersoft@gmail.com>
2020-06-10 15:04:56 +0000

    created footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,83 @@
+<section class="py-3" style="background: #f8f9fa">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="p-5">
+                    <h2 class="text-primary h2">GET OUR NEWSLETTER TO STAY UP TO DATE</h2>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="d-flex flex-wrap px-5">
+                    <form class="pb-5">
+                        <div class="form-row">
+                          <div class="col-12 col-md-4 mb-2 mb-md-0">
+                            <label class="sr-only" for="inlineFormInputGroup">FIRST NAME</label>
+                            <input type="text" class="form-control p-4 border-0 shadow-sm" placeholder="FIRST NAME">
+                          </div>
+                          <div class="col-12 col-md-4 mb-2 mb-md-0">
+                            <label class="sr-only" for="inlineFormInputGroup">LAST NAME</label>
+                            <input type="text" class="form-control p-4 border-0 shadow-sm" placeholder="LAST NAME">
+                          </div>
+                          <div class="col-12 col-md-4 mb-2 mb-md-0">
+                            <label class="sr-only" for="inlineFormInputGroup">EMAIL</label>
+                            <div class="input-group mb-2">
+                                <input type="email" class="form-control p-4 border-0 shadow-sm" id="inlineFormInputGroup" placeholder="EMAIL">
+                                <div class="input-group-append">
+                                    <div class="input-group-text text-white bg-blue"><i class="fa fa-at fa-2x" aria-hidden="true"></i></div>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-white py-3 tick-border-primary">
+    <div class="container py-4 mt-4">
+        <div class="row align-content-center">
+            <div class="col-12 col-md-3">
+                <div class="pb-3 mt-4">
+                    <img src="{{ asset('images/logo.png') }}" alt="" class="img-fluid rounded">
+                </div>
+            </div>
+            <div class="col-12 col-md-9 d-flex flex-wrap align-content-center justify-content-center" style="color: #0073C6">
+                <div class="d-flex flex-wrap justify-content-center">
+                    <div class="col-2 d-flex">
+                        <i class="fa fa-lightbulb-o fa-5x mr-2" aria-hidden="true"></i>
+                        Got a Greate Ideas
+                    </div>
+                    <div class="col-12 col-md-3 text-center p-3">
+                        <a href="#">WORK WITH US</a>
+                    </div>
+                    <div class="col-12 col-md-3 text-center p-3">
+                        <a href="#">CONTACT US</a>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="d-flex p-3 mb-5">
+                            <div class="col-3"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></div>
+                            <div class="col-3"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></div>
+                            <div class="col-3"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></div>
+                            <div class="col-3"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-wrap justify-content-around align-content-center flex-column flex-md-row" style="font-size: 16px">
+                    <span class="d-inline-block p-3 p-md-0 text-center mr-2">Reports<span class="ml-2 d-none d-md-inline">|</span></span><span class="d-inline-block p-3 p-md-0 text-center mr-2">Financial Overview<span class="ml-2 d-none d-md-inline">|</span></span><span class="d-inline-block p-3 p-md-0 text-center mr-2">Anti-corruption Policy<span class="ml-2 d-none d-md-inline">|</span></span><span class="d-inline-block p-3 p-md-0 text-center mr-2">Credit<span class="ml-2 d-none d-md-inline">|</span></span><span class="d-inline-block p-3 p-md-0 text-center mr-2">Privacy Policy<span class="ml-2 d-none d-md-inline">|</span></span><span class="d-inline-block p-3 p-md-0 text-center mr-2">Terms of Use<span class="ml-2 d-none d-md-inline">|</span></span><span class="d-inline-block p-3 p-md-0 text-center mr-2">Website by </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-blue">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="p-2 text-center text-white">
+                    <span>&copy; 2020 | <a href="#" class="text-white" style="font-size: 16px"><span>Poverty Watch Afirca</span> All Right Reserved</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
