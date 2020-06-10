681463f68a2de21c9b30696917436a1a2c0ec221
Stone Coder <stonecodersoft@gmail.com>
2020-06-10 15:47:23 +0000

    updated articles

--- database/migrations/2020_06_10_144227_create_articles_table.php
+++ database/migrations/2020_06_10_144227_create_articles_table.php
@@ -15,6 +15,19 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->text('intro');
+            $table->longText('content');
+            $table->longText('raw_html');
+            $table->string('image')->nullable();
+            $table->string('button_text')->nullable();
+            $table->string('link')->nullable();
+            $table->boolean('has_file')->default(false);
+            $table->string('file');
+            $table->string('icon')->nullable();
+            $table->unsignedBigInteger('section_id')->index();
+            $table->foreign('section_id')->references('id')->on('Sections')->onDelete('cascade');
             $table->timestamps();
         });
     }
