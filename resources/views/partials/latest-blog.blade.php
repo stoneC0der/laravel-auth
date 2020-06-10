aad6929266e18cc11c2ad2a18ab7f0cd1427e639
Stone Coder <stonecodersoft@gmail.com>
2020-06-10 15:05:09 +0000

    created latest blog

---
+++ resources/views/partials/latest-blog.blade.php
@@ -0,0 +1,67 @@
+    {{-- LATEST NEWS --}}
+    <section class="bg-white">
+        <svg viewBox="0 0 1800 268" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="Desktop" transform="translate(0.000000, -785.000000)" fill="#006DFF" stroke="#006DFF">
+                    <path d="M0.605680271,1054.59806 C0.605680271,1006.98697 -2.20658588,774.303455 -12.1204306,774.303455 C-144.855629,774.303455 471.598471,867.734989 1837.24187,1054.59806 L30.1754005,1054.59806 C28.4245375,1054.94291 22.7493572,1055.04456 16.9731399,1054.59806 C13.8721167,1054.35834 10.6866472,1048.89059 8.18783195,1054.59806 C2.06515104,1068.58269 0.605680271,1122.8904 0.605680271,1054.59806 Z" id="Path"></path>
+                </g>
+            </g>
+        </svg>
+    </section>
+    <section id="latest-news" class="pt-3 pb-5 bg-primar" style="background: #006DFF">
+        <div class="container bg-whit" style="background: #006DFF">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="py-2 mb-4">
+                        <h2 class="display-3 font-weight-bold text-center text-white">Latest News</h2>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="card overflow-hidden shadow-lg mb-3 mb-md-0g border-0 bg-white">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold text-primary h4">Card title</h5>
+                            <p class="card-text text-dark">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><span class="text-white">2020/10/05</span> <span><a href="http://#" class="pull-right card-link">VIEW DETAILS<i class="fa fa-arrow-right text-white ml-2" aria-hidden="true"></i></a></span></p>
+                        </div>
+                        <div class="overflow-hidden">
+                            <img src="{{ asset('images/3.png') }}" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="card shadow overflow-hidden border-0 mb-3 mb-md-0 bg-white">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold text-primary h4">Card title</h5>
+                            <p class="card-text text-dark">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><span class="text-white">2020/10/05</span> <span><a href="http://#" class="pull-right card-link">VIEW DETAILS<i class="fa fa-arrow-right text-white ml-2" aria-hidden="true"></i></a></span></p>
+                        </div>
+                        <div class="overflow-hidden">
+                            <img src="{{ asset('images/1.png') }}" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="card shadow overflow-hidden border-0 mb-3 mb-md-0">
+                        <div class="card-body bg-white">
+                            <h5 class="card-title font-weight-bold text-primary h4">Card title</h5>
+                            <p class="card-text text-dark">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><span class="text-white">2020/10/05</span> <span><a href="http://#" class="pull-right card-link">VIEW DETAILS<i class="fa fa-arrow-right text-white ml-2" aria-hidden="true"></i></a></span></p>
+                        </div>
+                        <div class="overflow-hidden">
+                            <img src="{{ asset('images/2.png') }}" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-blue">
+        <svg viewBox="0 0 1820 154" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="Desktop" transform="translate(0.000000, -711.000000)" fill="#f8f9fa" stroke="#f8f9fa">
+                    <path d="M1943.5185,701.589799 C2143.6435,684.8034 2249.60888,684.8034 2243.77107,701.589799 C2220.78292,767.691296 2187.40707,822.161363 2143.6435,865 L-47,865 C110.202474,846.636445 218.380542,841.541853 375.790513,827.093169 C921.451686,777.006849 1523.89309,736.787799 1943.5185,701.589799 Z" id="Path"></path>
+                </g>
+            </g>
+        </svg>
+    </section>
+    {{-- ./LATEST NEWS --}}
